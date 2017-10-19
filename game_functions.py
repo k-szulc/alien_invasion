5a11eaac552efb6e2b6cff1a61ca95ba99ff4f95
s-q-n <sqn@protonmail.com>
2017-10-19 16:55:13 +0200

    Fleet moving + bullets kills aliens

--- game_functions.py
+++ game_functions.py
@@ -45,12 +45,13 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     #Make most recently screen visible
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
 
     bullets.update()
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
 
@@ -86,3 +87,21 @@ def get_number_rows(ai_settings, ship_height, alien_height):
     available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
     number_rows = int(available_space_y / (2 * alien_height))
     return number_rows
+
+def check_fleet_edges(ai_settings, aliens):
+
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+def update_aliens(ai_settings, aliens):
+
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
--- settings.py
+++ settings.py
@@ -18,3 +18,8 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+
+        #Alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1 #1 = right; -1 left

9a97eb3fdae24ca07f38d38c46fe09aa484134d5
s-q-n <sqn@protonmail.com>
2017-11-08 18:50:18 +0100

    changed laser color

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ class Settings():
         #self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = 0, 255, 0
         self.bullets_allowed = 3
 
         #Alien settings
